8271deca292e0e70b6f0fbfb78754da0a742ff92
veronikapetrova358-ctrl <veronikapetrova358@gmail.com>
2026-01-28 15:12:16 +0200

    Add files via upload

---
+++ Вправа 4.4.1.py
@@ -0,0 +1,8 @@
+from tkinter import*
+root-Tk()
+root.geometry(400x300)
+
+entry=Entry(width = 25, bg = 'yellow', fg = 'blue', font = 'Arial 14', bd = 3)
+entry.insert(0, 'Вивчаємо властивості поля')
+entry.pack()
+root.mainloop()
