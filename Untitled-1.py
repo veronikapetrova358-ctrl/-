613f21a3baf3c1527ddd0d702646214b27b12ef0
veronikapetrova358-ctrl <veronikapetrova358@gmail.com>
2026-02-09 13:03:44 +0200

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,20 +1,35 @@
-from tkinter import *
-from tkinter.messagebox import * 
-def click(event) :
-    entry['bg'] = 'yellow'
-    entry['fg'] = 'red'
-    entry ['font'] = 12
-    label ['text'] = entry.get ()
-    x = entry.get()
-   showinfo ('Поле', x+ '!')
+from tkinter import*
 
-root = Tk()
-root.geometry ("300x250")
+def click () :
+    x = float (ent1.get())
+    y = float (ent2.get())
+    z = float (ent3.get())
+    t = x > 0 and (y > 0 or z < 0)
+    ent4.delete(0,END)
+    ent4.insert(0,str(t))
 
-entry = Entry ()
-entry.pack(pady = 20)
-entry.focus_set()
-entry.bind ('<1>',click)
+win = Tk()
+win.geometry('300x500')
 
-label = Label ()
-label.pack (pady=10)
+lb1 = Label(text = 'Значення змінної х')
+lb1.pack(pady =5)
+ent1 = Entry()
+ent1.pack(pady=5)
+
+lb2 = Label(text 'Значення змінної y')
+lb2.pack(pady =5)
+ent2 = Entry
+ent2.pack(pady=5)
+
+lb3 = Label(text 'Значення змінної z')
+lb3.pack(pady =5)
+ent3 = Entry
+ent3.pack(pady=5)
+
+bt = Button(text = 'Обчислити', command = click)
+bt.pack(pady =5)
+
+lb4 = Label(text 'Результат')
+lb4.pack(pady =5)
+ent4 = Entry
+ent4.pack(pady=5)
